a406df454bc01726e0687eb15f335d69973236bf
Pedro Endo <endopedro@gmail.com>
2021-08-16 17:46:39 -0300

    feat: add home link to logo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import { Disclosure } from '@headlessui/react'
 
 import { navItems } from './items'
@@ -16,7 +17,9 @@ const Navbar = ({ page, blogName }) => (
             </div>
             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex-shrink-0 flex items-center">
-                <h2 className="text-2xl font-bold">{blogName}</h2>
+                <Link href={'/'}>
+                  <a className="text-2xl font-bold">{blogName}</a>
+                </Link>
               </div>
               <div className="hidden sm:block sm:ml-6">
                 <div className="flex space-x-4">
