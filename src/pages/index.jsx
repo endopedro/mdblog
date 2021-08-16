6b6e62aa477061623f2952bf54512f3e3fafc20e
Pedro Endo <endopedro@gmail.com>
2021-08-16 18:33:29 -0300

    feat: blog hero component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+
 import Layout from '../components/Layout'
+import Hero from '../components/Hero'
 
 const Home = () => {
-  return <Layout blogName="Minimalister"></Layout>
+  return (
+    <Layout blogName="Minimalister">
+      <Hero />
+    </Layout>
+  )
 }
 
 export default Home
