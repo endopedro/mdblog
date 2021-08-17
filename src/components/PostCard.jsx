acdb7ee6a1c36c1dd6658c58a971ea05daaf557f
Pedro Endo <endopedro@gmail.com>
2021-08-17 12:23:41 -0300

    feat: post card

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const PostCard = ({ className }) => {
+  return (
+    <div className={className}>
+      <img
+        className="h-40 w-full object-cover"
+        src="https://picsum.photos/400"
+        alt=""
+      />
+      <div className="bg-woodsmoke-500 py-5 px-7 shadow">
+        <p className="uppercase text-xs font-semibold text-gray-500 mb-3">
+          October 31, 2018
+        </p>
+        <h2 className="text-xl font-bold mb-3">Standard post format</h2>
+        <p className="text-sm leading-relaxed mb-5">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus…. Read more
+        </p>
+        <div className="flex items-center">
+          <img
+            className="rounded-full h-10 w-10 object-cover mr-2"
+            src="https://picsum.photos/100"
+            alt=""
+          />
+          <h5 className="uppercase text-xs text-gray-500">ALEXEY TROFIMOV</h5>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PostCard
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
+import PostCard from '../components/PostCard'
 
 const Home = () => {
   return (
     <Layout blogName="Minimalister">
-      <Hero />
+      <Hero className="mt-7 mb-10" />
+      <div className="grid md:grid-cols-2 md:gap-10">
+        {[1, 2, 3].map((item) => (
+          <PostCard className="mb-10 md:mb-0" />
+        ))}
+      </div>
     </Layout>
   )
 }
