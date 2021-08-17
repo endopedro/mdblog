033618692315947e224c060b6b88f38145a71e17
Pedro Endo <endopedro@gmail.com>
2021-08-17 12:23:47 -0300

    fix: spacing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
-import cx from 'classnames'
 
 const Hero = ({ className }) => {
   return (
-    <div
-      className={cx(
-        className,
-        'h-64 bg-white relative flex items-end rounded-sm overflow-hidden'
-      )}
-    >
-      <img
-        className="object-cover h-full w-full absolute"
-        src="https://picsum.photos/800/500"
-      />
-      <div className="relative w-full h-full p-5 flex justify-end flex-col">
-        <div className="bg-woodsmoke-500 py-5 px-7 shadow md:w-max rounded-sm mb-2">
-          <p className="text-lg md:text-3xl font-bold ">
-            Wellcome to MINIMALISTER!
-          </p>
-        </div>
-        <div className="bg-woodsmoke-500 py-5 px-7 shadow rounded-sm md:w-max">
-          <p className="">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac
-            ipsum odio.
-          </p>
+    <div className={className}>
+      <div className="h-64 bg-white relative flex items-end">
+        <img
+          className="object-cover h-full w-full absolute"
+          src="https://picsum.photos/800/500"
+        />
+        <div className="relative w-full h-full p-5 flex justify-end flex-col">
+          <div className="bg-woodsmoke-500 py-5 px-7 shadow md:w-max mb-2">
+            <p className="text-lg md:text-3xl font-bold ">
+              Wellcome to MINIMALISTER!
+            </p>
+          </div>
+          <div className="bg-woodsmoke-500 py-5 px-7 shadow md:w-max">
+            <p className="">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac
+              ipsum odio.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = ({ children, page, blogName }) => {
       </Head>
       <main>
         <Navbar page={page} blogName={blogName} />
-        <div className="container mx-auto px-2 lg:px-8 max-w-4xl">
+        <div className="container mx-auto md:px-2 lg:px-8 max-w-4xl">
           {children}
         </div>
       </main>
