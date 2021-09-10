ebd6d9ad6cde8c764831e8f03b6c4b03373ba640
Pedro Endo <endopedro@gmail.com>
2021-09-10 00:43:57 -0300

    improvement: dynamic medium post card

--- src/components/PostCard/Medium.jsx
+++ src/components/PostCard/Medium.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
-const MediumPostCard = () => {
+const MediumPostCard = ({ cover, slug, author, title }) => {
   return (
-    <>
-      <div className="h-52 w-full relative">
-        <img
-          className="h-full w-full object-cover"
-          src="https://picsum.photos/400"
-          alt=""
-        />
-        <div className="absolute h-full w-full top-0 opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
-          <div className="bg-woodsmoke-500 p-4 shadow absolute bottom-5">
-            <h3 className=" font-bold text-md mb-3">Standard post format</h3>
-            <h5 className="text-xs text-gray-500">
-              <span className="font-bold">by </span>
-              <span className="uppercase">ALEXEY TROFIMOV</span>
-            </h5>
-          </div>
+    <div className="h-52 w-full relative">
+      <Image src={cover.secure_url} layout="fill" objectFit="cover" />
+      <div className="absolute h-full w-full top-0 opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
+        <div className="bg-woodsmoke-500 p-4 shadow absolute bottom-5">
+          <Link href={`/post/${slug}`}>
+            <a className="cursor-pointer hover:underline">
+              <h3 className=" font-bold text-md mb-3">{title}</h3>
+            </a>
+          </Link>
+          <h5 className="text-xs text-gray-500">
+            <span className="font-bold">by </span>
+            <span className="uppercase">{author.name}</span>
+          </h5>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/PostCard/Small.jsx
+++ src/components/PostCard/Small.jsx
@@ -6,30 +6,28 @@ import formatDate from '../../utils/formatDate'
 
 const SmallPostCard = ({ cover, slug, author, title, createdAt }) => {
   return (
-    <>
-      <div className="flex mx-3 sm:mx-0">
-        <Link href={`/post/${slug}`}>
-          <div className="h-24 w-1/3 flex-shrink-0 relative mr-3 cursor-pointer">
-            <Image src={cover.secure_url} layout="fill" objectFit="cover" />
-            <div className="absolute h-full w-full top-0 bg-black opacity-0 hover:opacity-50 transition duration-300 ease-in-out" />
-          </div>
-        </Link>
-        <div className="h-24 w-full flex flex-col justify-center">
-          <Link href={`/post/${slug}`}>
-            <a className="cursor-pointer hover:underline">
-              <h3 className="text-md font-bold mb-3">{title}</h3>
-            </a>
-          </Link>
-          <h5 className="text-xs text-gray-500">
-            <span className="font-bold">by </span>
-            <span className="uppercase">{author.name}</span>
-          </h5>
-          <p className="text-xs font-semibold text-gray-500">
-            {formatDate(createdAt)}
-          </p>
+    <div className="flex mx-3 sm:mx-0">
+      <Link href={`/post/${slug}`}>
+        <div className="h-24 w-1/3 flex-shrink-0 relative mr-3 cursor-pointer">
+          <Image src={cover.secure_url} layout="fill" objectFit="cover" />
+          <div className="absolute h-full w-full top-0 bg-black opacity-0 hover:opacity-50 transition duration-300 ease-in-out" />
         </div>
+      </Link>
+      <div className="h-24 w-full flex flex-col justify-center">
+        <Link href={`/post/${slug}`}>
+          <a className="cursor-pointer hover:underline">
+            <h3 className="text-md font-bold mb-3">{title}</h3>
+          </a>
+        </Link>
+        <h5 className="text-xs text-gray-500">
+          <span className="font-bold">by </span>
+          <span className="uppercase">{author.name}</span>
+        </h5>
+        <p className="text-xs font-semibold text-gray-500">
+          {formatDate(createdAt)}
+        </p>
       </div>
-    </>
+    </div>
   )
 }
 
