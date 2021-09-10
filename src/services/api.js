73385f1e89c55b37b9bf0994aaa7e42ae12ee612
Pedro Endo <endopedro@gmail.com>
2021-09-10 16:03:03 -0300

    refact: api service

--- src/services/api.js
+++ src/services/api.js
@@ -5,20 +5,12 @@ axios.defaults.paramsSerializer = (params) =>
   qs.stringify(params, { arrayFormat: 'brackets' })
 
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_SITE_URL}/api`,
+  baseURL: process.env.BLOG_API,
 })
 
 const appApi = () => ({
   getPost: (slug) => api.get('/posts', { params: { slug: slug } }),
   getPosts: () => api.get('/posts'),
-  getCategory: (id) => api.get('/categories', { params: { _id: id } }),
-  getCategories: () => api.get('/categories'),
-  getPage: (slug) => api.get('/pages', { params: { slug: slug } }),
-  getPages: () => api.get('/pages'),
-  getSettings: () => api.get('/settings'),
-  getMetrics: () => api.get('/settings', { params: { metrics: true } }),
-  getUsers: () => api.get('/users'),
-  getUser: (id) => api.get('/users', { params: { _id: id } }),
 })
 
 export { appApi }
