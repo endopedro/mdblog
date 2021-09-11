5b02392d79b837f91702145eb978bd4683c1955c
Pedro Endo <endopedro@gmail.com>
2021-09-11 17:47:29 -0300

    feat: dynamic main cover image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Hero = ({ className, settings }) => {
   return (
     <div className={className}>
-      <div className="h-64 relative flex items-end">
-        <img
-          className="object-cover h-full w-full absolute opacity-30"
-          src="https://picsum.photos/800/500"
+      <div className="h-64 w-full relative flex items-end">
+        <Image
+          src={settings.cover.secure_url}
+          layout="fill"
+          objectFit="cover"
         />
         <div className="relative w-full h-full p-5 flex justify-end flex-col">
           <div className="bg-woodsmoke-500 py-5 px-7 shadow md:w-max mb-2">
