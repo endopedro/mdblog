36ba521d7c38dfd30190df02d85f552e3f1459be
Pedro Endo <endopedro@gmail.com>
2021-09-18 01:54:08 -0300

    feat: home title link style

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -20,7 +20,9 @@ const Navbar = ({ page, settings }) => (
             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex-shrink-0 flex items-center">
                 <Link href={'/'}>
-                  <a className="text-2xl font-bold">{settings.name}</a>
+                  <a className="text-2xl font-bold hover:text-blue-400 transition duration-300">
+                    {settings.name}
+                  </a>
                 </Link>
               </div>
               <div className="hidden sm:block sm:ml-6">
