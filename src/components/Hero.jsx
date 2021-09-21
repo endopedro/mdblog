e683de07151902c61dc847805e1743302c20eaf0
Pedro Endo <endopedro@gmail.com>
2021-09-21 14:57:24 -0300

    refact: hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
 
+import Portlet, { PortletFooter } from '../components/Portlet'
+
 const Hero = ({ className, cover, title, subtitle }) => (
-  <div className={className}>
+  <Portlet className={className}>
     <div className="h-64 w-full relative flex items-end">
       <Image
         className="opacity-30"
@@ -12,18 +14,19 @@ const Hero = ({ className, cover, title, subtitle }) => (
       />
       <div className="relative w-full h-full p-5 flex justify-end flex-col">
         {title && (
-          <div className="bg-woodsmoke-500 py-5 px-7 shadow md:w-max mb-2">
+          <div className="bg-woodsmoke-500 py-5 px-7 shadow md:w-max mb-2 rounded-lg">
             <p className="text-lg md:text-3xl font-bold">{title}</p>
           </div>
         )}
-        {subtitle && (
+        {subtitle && false && (
           <div className="bg-woodsmoke-500 py-5 px-7 shadow md:w-max">
             <p className="">{subtitle}</p>
           </div>
         )}
       </div>
     </div>
-  </div>
+    {subtitle && <PortletFooter>{subtitle}</PortletFooter>}
+  </Portlet>
 )
 
 export default Hero
