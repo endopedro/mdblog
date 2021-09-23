0601ae21811a01a123ed75f7af2317b69756ac00
Pedro Endo <endopedro@gmail.com>
2021-09-23 02:22:00 -0300

    fix: convert about page into a generic page

--- src/pages/[slug].jsx
+++ src/pages/[slug].jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
@@ -7,7 +8,7 @@ import Portlet, { PortletBody } from '../components/Portlet'
 import MdContent from '../components/MdContent'
 import { appApi } from '../services/api'
 
-const about = ({ page, settings }) => (
+const about = ({ page, settings, author }) => (
   <Layout settings={settings} page={page.slug}>
     <Hero cover={page.cover.secure_url} title={page.title} className="my-7" />
     <Portlet className="mb-7">
@@ -15,6 +16,32 @@ const about = ({ page, settings }) => (
         <MdContent children={page.content} />
       </PortletBody>
     </Portlet>
+    {page.slug == 'about' && (
+      <Portlet className="mb-10">
+        <div className="flex items-center">
+          <div className="h-16 w-16 md:h-20 md:w-20 mr-5">
+            <Image
+              src={author.picture.secure_url}
+              height={80}
+              width={80}
+              objectFit="cover"
+              className="border border-woodsmoke-50"
+            />
+          </div>
+
+          <div className="flex justify-center flex-col">
+            <h4 className="text-sm font-bold">This blog is sustained by</h4>
+            <Link href={`/author/${author.username}`} passHref>
+              <a>
+                <h5 className="uppercase hover:text-blue-400 transition duration-300">
+                  {author.name}
+                </h5>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </Portlet>
+    )}
   </Layout>
 )
 
@@ -28,6 +55,10 @@ export async function getStaticProps(context) {
       .getSettings()
       .then(({ data }) => data.result)
       .catch((err) => null),
+    appApi()
+      .getMainUser()
+      .then(({ data }) => data.result)
+      .catch((err) => null),
   ]).then((values) => values)
 
   if (!data[0]) return { notFound: true }
@@ -36,6 +67,7 @@ export async function getStaticProps(context) {
     props: {
       page: data[0],
       settings: data[1],
+      author: data[2],
     },
   }
 }
